29fcc18a4fc30f741b8c0d862d8ff648a5945b41
EdytaCiesielska <97843261+EdytaCiesielska@users.noreply.github.com>
2022-07-08 20:33:28 +0200

    Delete medclinic directory

--- medclinic/PatientRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.devopsi.akademia.medclinic;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRepository extends JpaRepository<Patient, String> {
-}
