ec602e5f3ab6afecdcb17c4f249c3f3ff7ec0dfa
EdytaCiesielska <97843261+EdytaCiesielska@users.noreply.github.com>
2022-07-08 20:30:24 +0200

    Add files via upload

---
+++ medclinic/MedclinicApplication.java
@@ -0,0 +1,13 @@
+package pl.devopsi.akademia.medclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedclinicApplication.class, args);
+    }
+
+}
